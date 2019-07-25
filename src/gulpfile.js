36777c47d09a5fc2b929d8fb51f38f2eda62a041
yukihiro <epirevolve@gmail.com>
2019-07-25 16:39:20 +0900

    can exec by shell

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -7,10 +7,16 @@ const config = require('/src/webpack.config.js');
 const process = require('process');
 process.chdir('/src');
 
+const exec = require('child_process').exec;
+
 // jsx babel
 gulp.task('babel', () => webpackStream(config, webpack).pipe(gulp.dest('.')));
 
 // change detection
 gulp.task('watch', () => gulp.watch([ '/src/**/*.js', '/src/**/*.jsx', '!/src/**/*.min.js' ],
     {interval: 1000, usePolling: true, delay: 3000},
-    gulp.series('babel')));
+    exec('gulp babel', (err, stdout, stderr) => {
+        if (err) { console.log(err); }
+        console.log(stdout);
+    })
+));
